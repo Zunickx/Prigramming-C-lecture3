68a54bc63944a8d2bc5b0278ff1c4f72c07f3e84
Zunickx <Zunickx@gmail.com>
2022-08-14 15:58:35 +0300

    Task001 Цикл For

---
+++ Task001_cycle/Program.cs
@@ -0,0 +1,12 @@
+﻿string Metod4(int count, string c)
+{
+    string result = "";
+    for(int i = 0; i < count; i++)
+    {
+        result = result + c;
+    }
+    return result;
+}
+
+string res = Metod4(10, "coc");
+Console.WriteLine(res);
